078feeda5789252614645d2e62033adb7d88796e
Aditya Dhakal <dhakaladitya96@gmail.com>
2024-06-19 22:59:12 +0545

    controllers and ui

--- backend/models/item.js
+++ backend/models/item.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const env = require('dotenv').config();
 
 const itemSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -8,6 +10,23 @@ const itemSchema = new mongoose.Schema({
     age: { type: Number, required: true },
     goal: { type: String, required: true }
 });
+
+itemSchema.methods.generateAccessToken = function () {
+    return (jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+
+            name: this.name
+        },
+        process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+    )
+
+}
+
 
 const itemModel = mongoose.model('Item', itemSchema);
 
